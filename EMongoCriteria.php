d7d758b949cefae6055eece49cdbcd8942ad966e
Steven Hadfield <steven.hadfield@business.com>
2014-01-21 13:45:59 -0800

    Update list of EMongoCriteria operators to include missing geo-spatial and logical operators

--- EMongoCriteria.php
+++ EMongoCriteria.php
@@ -63,7 +63,14 @@ class EMongoCriteria extends CComponent
 		'eq'			=> '$$eq',
 		'=='			=> '$$eq',
 		'where'			=> '$where',
-		'or'			=> '$or'
+		'or'			=> '$or',
+		'nor'           => '$nor',
+		'not'           => '$not',
+		'near'          => '$near',
+		'nearsphere'    => '$nearSphere',
+		'geowithin'     => '$geoWithin',
+		'geointersects' => '$geoIntersects',
+		'type'          => '$type',
 	);
 
 	const SORT_ASC		= 1;
@@ -452,19 +459,19 @@ class EMongoCriteria extends CComponent
 	public function addCond($fieldName, $op, $value)
 	{
 		$op = self::$operators[$op];
-		
-		if($op == self::$operators['or']) 
+
+		if($op == self::$operators['or'])
 		{
-			if(!isset($this->_conditions[$op])) 
+			if(!isset($this->_conditions[$op]))
 			{
 				$this->_conditions[$op] = array();
 			}
 			$this->_conditions[$op][] = array($fieldName=>$value);
 		} else {
-		
+
 			if(!isset($this->_conditions[$fieldName]) && $op != self::$operators['equals'])
 				$this->_conditions[$fieldName] = array();
-	
+
 			if($op != self::$operators['equals'])
 			{
 				if(
